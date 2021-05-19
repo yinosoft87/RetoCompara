036739a251d58f06a60e9753f4c8a06f07831f5f
Yinosoft87 <yedid_yah@hotmail.com>
2021-05-19 09:11:36 -0700

    iniciando carga del proyecto al git

---
+++ src/main/java/com/reto/compare/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.reto.compare;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CompareApplication.class);
+	}
+
+}
